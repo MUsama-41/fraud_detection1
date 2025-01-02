2cc9fc92ae2006d8b2d72aa0bac1d6d4dc958e70
Dev <musama.afzal@devsinc.com>
2025-01-02 09:58:37 -0500

    updated app.py in main branch

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("app.py")
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+model = joblib.load('fraud_model.pkl')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    features = np.array(data['features']).reshape(1, -1)
+    prediction = model.predict(features)
+    probability = model.predict_proba(features)[0][1]
+    return jsonify({'fraud_prediction': int(prediction[0]), 'fraud_probability': probability})
